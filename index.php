bb5dedc3aa580857af88a71095caab04946f9600
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-04-14 15:07:00 +0200

    class method creation

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 
 class Movie {
+    // Class Attributes
     public $title;
     public $year;
     public $length;
     public $genre;
+    public $vote;
 
+    // Class methods
+    public function setVote() {
+        $this -> vote = rand(0, 100);
+    }
+
+    // Class Constructs
     function __construct($title, $year, $length, $genre) {
         $this -> title = $title;
         $this -> year = $year;
